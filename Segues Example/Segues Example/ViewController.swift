8240f2a37f0aba68197b5bb16bebefd5ed5edbae
이동혁 <LeeDH@idonghyeog-ui-MacBook-Pro-2.local>
2015-05-12 00:57:39 +0900

    Segues Example

---
+++ Segues Example/Segues Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Segues Example
+//
+//  Created by 이동혁 on 2015. 5. 2..
+//  Copyright (c) 2015년 CodeMath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
